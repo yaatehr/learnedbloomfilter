957c972f4b4f274c0f2e64a488a97346b432655f
Yaateh Richardson <yaatehr@mit.edu>
2020-04-18 02:27:30 -0400

    make fpr to size charts

--- python/scripts/visualize_benchmarks_2.py
+++ python/scripts/visualize_benchmarks_2.py
@@ -4,89 +4,72 @@ import matplotlib.pyplot as plt
 import sqlite3 as sql
 import pandas as pd
 from math import log10, floor
-
+import os
 
 round_to_n = lambda x, n: round(x, -int(floor(log10(abs(x)))) + (n - 1))
 
 # test_name = "explicit_always_false"
-test_name = "explicit_lstm_1"
+test_name = "explicit_lstm_2"
 fixture_data = pd.read_csv(f"{test_name}_normalized.csv", index_col=False)
 
-fixture_data["calibration_fpr"] = fixture_data.inv_max_fpr.astype(float)**-1
+fixture_data["compound_size"] = fixture_data["table_size"] + fixture_data["lbf_size"]
 # columns = ["function", "projected_eles", "inv_max_fpr", "actual_fpr", "num_hashes", "table_size", "batch_size", "len_gen_eles", "iterations", "real_time", "cpu_time"]
-columns = ["function", "actual_fpr", "num_hashes", "calibration_fpr", "table_size", "batch_size"]
+columns = ["function", "actual_fpr", "num_hashes", "target_fpr", "tau", "table_size", "compound_size"]
 fixture_data = fixture_data[columns]
-# print(fixture_data)
+
+print(fixture_data.head(10))
 
 
 # string_query = fixture_data[(fixture_data.function.str.contains("TestBloomFilterStringQuery"))]
-# string_insertion = fixture_data[(fixture_data.function.str.contains("TestBloomFilterStringInsertion"))]
+# string_insertion = fixture_data[(fixture_data.function.str.contains("TlbestBloomFilterStringInsertion"))]
 # int_insertion = fixture_data[(fixture_data.function.str.contains("TestBloomFilterIntInsertion"))]
 # int_query = fixture_data[(fixture_data.function.str.contains("TestBloomFilterIntQuery"))]
 
+gbf_data = fixture_data[(fixture_data["tau"] ==1)]
+gbf_data = gbf_data.sort_values("table_size")
+lbf_data = fixture_data[(fixture_data["tau"] !=1)]
 
 # we want to gorup by batch_size because fprs are comperable
 # fpr==num_hashes should be shown against the table size
 # batch-size, calculated_fpr, then map table size against acutal fpr
 
-queries = [fixture_data]
+queries = [lbf_data]
 querie_name = ["string_query"]
 # sort_by = ["projected_eles", "fpr", "pos_eles", "ele_length"]
 sort_by = ["projected_eles", "fpr", "pos_eles"]
 time_aggregate = ["real_time", "cpu_time"]
 colors = ["b", "c", "g", "y", "r", "m", "k"]
-unique_hashes = fixture_data.calibration_fpr.unique()
-unique_hashes.sort()
+unique_tau = fixture_data.tau.unique()
+# unique_hashes.sort()
 # print(unique_hashes)
-colormap = dict(zip(unique_hashes, colors[: len(unique_hashes)]))
+colormap = dict(zip(unique_tau, colors[: len(unique_tau)]))
+if not os.path.exists(f"laptop_benchmarks/{test_name}"):
+    os.makedirs(f"laptop_benchmarks/{test_name}")
 
 for ind, query in enumerate(queries): 
-    for group1_label, group1 in query.groupby("batch_size"):
-        g = group1.sort_values("actual_fpr")
-        # if(len(g.actual_fpr.unique()) == 1):
-        #     continue
-
+    for group1_label, group1 in query.groupby("tau"):
+        g = group1.sort_values("compound_size")
         fig, ax = plt.subplots()
-        for group2_label, group2 in g.groupby("calibration_fpr"):
-                group2_label_r = round_to_n(group2_label, 3)
-                ax.plot(group2.actual_fpr, group2.table_size, marker='o', linestyle='', label=group2_label_r, color=colormap[group2_label])
+        ax.plot(g.compound_size, g.actual_fpr, marker='.', linestyle='-', label=group1_label, color="b")
+        ax.plot(gbf_data.table_size, gbf_data.actual_fpr, marker='.', linestyle='-', label="Generic BF", color="r")
         ax.legend()
-        ax.title.set_text("%s, batch_size: %d" % (test_name, group1_label))
-        ax.set_ylabel("table size (bytes)")
-        plt.savefig(f"laptop_benchmarks/{test_name}/batch_size-{group1_label}.png")
+        ax.title.set_text("%s, tau: %d" % (test_name, group1_label))
+        ax.set_ylabel("Empirical FPR")
+        ax.set_xlabel("Table Size (bytes)")
+        plt.savefig(f"laptop_benchmarks/{test_name}/tau-{group1_label}.png")
         plt.clf()
         plt.close()
-    for group1_label, group1 in query.groupby("batch_size"):
-        g = group1.sort_values("actual_fpr")
-        # if(len(g.actual_fpr.unique()) == 1):
-        #     continue
 
+    for group1_label, group1 in query.groupby("tau"):
+        g = group1.sort_values("compound_size")
         fig, ax = plt.subplots()
-        for group2_label, group2 in g.groupby("calibration_fpr"):
-                group2_label_r = round_to_n(group2_label, 3)
-
-                ax.plot(group2.actual_fpr, group2.table_size, marker='o', linestyle='', label=group2_label_r, color=colormap[group2_label])
+        ax.plot(g.compound_size, g.actual_fpr, marker='.', linestyle='-', label=group1_label, color="b")
+        ax.plot(gbf_data.table_size, gbf_data.actual_fpr, marker='.', linestyle='-', label="Generic BF", color="r")
         ax.legend()
-        ax.set_yscale("log")
-        ax.title.set_text("%s, batch_size: %d" % (test_name, group1_label))
-        ax.set_ylabel("table size (bytes)")
-        plt.savefig(f"laptop_benchmarks/{test_name}/batch_size-{group1_label}-logy.png")
+        ax.title.set_text("%s, tau: %d" % (test_name, group1_label))
+        ax.set_ylabel("Empirical FPR")
+        ax.set_xscale("log")
+        ax.set_xlabel("Table Size (bytes)")
+        plt.savefig(f"laptop_benchmarks/{test_name}/tau-{group1_label}-log.png")
         plt.clf()
         plt.close()
-    for group1_label, group1 in query.groupby("batch_size"):
-        g = group1.sort_values("actual_fpr")
-        # if(len(g.actual_fpr.unique()) == 1):
-        #     continue
-
-        for group2_label, group2 in g.groupby("calibration_fpr"):
-                group2_label_r = round_to_n(group2_label, 3)
-
-                fig, ax = plt.subplots()
-                ax.plot(group2.actual_fpr, group2.table_size, marker='o', linestyle='', label=group2_label_r, color=colormap[group2_label])
-                # ax.legend()
-                ax.set_yscale("log")
-                ax.title.set_text("%s, batch_size: %d" % (test_name, group1_label))
-                ax.set_ylabel("table size (bytes)")
-                plt.savefig(f"laptop_benchmarks/{test_name}/batch_size-{group1_label}-frp-{group2_label_r}-logy.png")
-                plt.clf()
-                plt.close()
