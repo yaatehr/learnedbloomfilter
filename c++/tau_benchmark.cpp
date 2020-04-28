49258d9703050b115bda36822c56a99da76f1c6d
Yaateh Richardson <yaatehr@mit.edu>
2020-04-28 11:31:50 -0400

    debug

--- c++/tau_benchmark.cpp
+++ c++/tau_benchmark.cpp
@@ -166,12 +166,19 @@ BENCHMARK_DEFINE_F(MyFixtureLearned, TestBloomFilterStringQuery)
             // st.counters["lbf_size"] = std::move(COMPOUND_MODEL_SIZE);
             // st.counters["target_fpr"] =  std::move(MyFixtureLearned::fpr[st.range(0)]);
 
-            st.counters[std::string("fpr")] = std::move(exp_fpr);
-            st.counters[std::string("num_hashes")] = std::move(num_hashes);
-            st.counters[std::string("table_size")] = std::move(table_size);
-            st.counters[std::string("tau")] = std::move(round_to_digits(tau[st.range(1)], 3));
-            st.counters[std::string("lbf_size")] = std::move((double)COMPOUND_MODEL_SIZE);
-            st.counters[std::string("target_fpr")] =  std::move(round_to_digits(MyFixtureLearned::fpr[st.range(0)], 3));
+            // st.counters[std::string("fpr")] = std::move(exp_fpr);
+            // st.counters[std::string("num_hashes")] = std::move(num_hashes);
+            // st.counters[std::string("table_size")] = std::move(table_size);
+            // st.counters[std::string("tau")] = std::move(round_to_digits(tau[st.range(1)], 3));
+            // st.counters[std::string("lbf_size")] = std::move((double)COMPOUND_MODEL_SIZE);
+            // st.counters[std::string("target_fpr")] =  std::move(round_to_digits(MyFixtureLearned::fpr[st.range(0)], 3));
+            
+            st.counters[std::string("fpr")] = exp_fpr;
+            st.counters[std::string("num_hashes")] = num_hashes;
+            st.counters[std::string("table_size")] = table_size;
+            st.counters[std::string("tau")] = round_to_digits(tau[st.range(1)], 3);
+            st.counters[std::string("lbf_size")] = (double)COMPOUND_MODEL_SIZE;
+            st.counters[std::string("target_fpr")] =  round_to_digits(MyFixtureLearned::fpr[st.range(0)], 3);
 
       }
 }
